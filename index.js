f73a08be4bff951ae6cdb4cea2310ceb16e1cf17
Kunal <ks231229@gmail.com>
2023-08-31 18:01:52 +0530

    my first production release

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require ("express");
+const format = require ("date-format");
+const app = express();
+const PORT = 4000 || process.env.PORT;
+
+app.get("/",(req,res) => {
+    res.status(201).send("<h1> Hello World Lco </h1>");
+});
+
+app.get("/api/v1/instagram",(req,res) =>{
+    const instaSocial = {
+        username : " kunal_r_singh",
+        followers : 170,
+        following : 190,
+        date : format.asString("dd MM hh mm",new Date()),
+
+    };
+
+    res.status(200).json(instaSocial);
+});
+
+app.get("/api/v1/facebook",(req,res) =>{
+    const facebook ={
+        username : " kunal_r_singh",
+        freinds : 500,
+        post : 10,
+        date : format.asString("dd MM - hh mm",new Date()),
+    };
+    res.status(200).json({facebook});
+});
+app.get("/api/v1/:token",(req,res)=>{
+  //  console.log(req.params.token);
+    res.status(200).json({param1: req.params.token });
+});
+
+app.get("/api/v1/:token/:id",(req,res)=>{
+  //  console.log(req.params.token);
+    res.status(200).json({param1: req.params.token, param2: req.params.id});
+});
+
+app.listen(PORT,()=> {
+    console.log(`Server is running at ${PORT}`);
+});
