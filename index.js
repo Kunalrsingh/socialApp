4f53325f0f3ea4f915ddd16d3cfbd5e706b9b217
Kunal <ks231229@gmail.com>
2023-09-01 22:00:15 +0530

    added yaml file and nodemon file

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require ("express");
 const format = require ("date-format");
 const app = express();
+
+// swagger docs
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument= YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const PORT = process.env.PORT|| 4000  ;
 
 app.get("/",(req,res) => {
